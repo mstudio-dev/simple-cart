45c3ece69e37cb2480e58b878ba287c37b84689c
mstudio-dev <mstudio-dev@users.noreply.github.com>
2024-07-10 18:12:36 +0200

    Update config.php

--- system/modules/shopping_cart/config/config.php
+++ system/modules/shopping_cart/config/config.php
@@ -1,1 +1,14 @@
+<?php
+
+$GLOBALS['BE_MOD']['content']['shopping_cart'] = array(
+    'tables' => array('tl_product', 'tl_category'),
+    'icon'   => 'system/modules/shopping_cart/assets/icon.png',
+);
+
+$GLOBALS['FE_MOD']['shopping_cart'] = array(
+    'shopping_cart' => 'ModuleShoppingCart',
+);
+
+$GLOBALS['TL_MODELS']['tl_product'] = 'MyVendor\ShoppingCart\Model\ProductModel';
+$GLOBALS['TL_MODELS']['tl_category'] = 'MyVendor\ShoppingCart\Model\CategoryModel';
 
